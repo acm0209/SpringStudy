ae818abc3cb009abb39cb6d0885c0e8064f7003e
acm0209 <acm0209@naver.com>
2023-02-25 17:37:54 +0900

    JPA, DB, Test

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,4 +1,4 @@
-package jpahook.jpashop;
+package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,4 +1,4 @@
-package jpahook.jpashop;
+package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
