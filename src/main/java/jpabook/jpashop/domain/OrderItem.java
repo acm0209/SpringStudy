1b58855bb602c2029166e57ff6b760fe332392ee
acm0209 <acm0209@naver.com>
2023-02-27 16:23:27 +0900

    [REFACTOR] JPA

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,20 +6,24 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class OrderItem {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
-    @JoinColumn(name="order_id")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice; // 주문 가격
